f60439f11f6f56a56ab7ce46698e4098243e9bcb
Tanmayg19 <tanmay.gautam10@gmail.com>
2026-06-17 20:36:11 +0530

    Update Amazon_sales.sql

--- Amazon_sales.sql
+++ Amazon_sales.sql
@@ -1,3 +1,19 @@
+####### Amazon Sales Data Analysis ######
+-- Overview of Dataset --
+-- The data consists of sales record of three cities/branch in Myanmar 
+-- which are Naypyitaw, Yangon, Mandalay which took place in first quarter of year 2019 --
+-- the data consists of 1000 rows and 17 columns --
+
+
+-- Objective of Project --
+-- The major aim of this project is to gain insight into the sales data of Amazon --
+-- and to understand the different factors that affect sales of the different branches --
+#-------------------------------------------------------------------------------------------------------#
+
+-- Data Wrangling--
+-- step.1] creating database and importing data using table data import wizard --
+create database amazon;
+
 CREATE TABLE sales (
 Invoice_ID VARCHAR(12) PRIMARY KEY,
 Branch VARCHAR(5),
